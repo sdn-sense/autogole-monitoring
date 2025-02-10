07562e1c7ca7783576a463d8e43bfa7765dce5be
Justas Balcas <juztas@gmail.com>
2025-02-10 08:53:45 -0600

    Update to use text and content-type

--- kubernetes/xrootd/build/xrootd-mon/config/default/opt/http-server.py
+++ kubernetes/xrootd/build/xrootd-mon/config/default/opt/http-server.py
@@ -1,16 +1,19 @@
+"""Simple http server to serve metrics from file"""
 import os.path
+from flask import Flask, Response
 
-from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    """Main page hello world"""
     return 'Hello! Go to /metrics to see the metrics.'
 
 @app.route('/metrics')
 def metrics():
+    """Return metrics from file"""
     if os.path.isfile('/srv/xrootd-metrics'):
         with open('/srv/xrootd-metrics', 'rb') as fd:
             content = fd.read()
-        return content.decode('utf-8')
-    raise FileNotFoundError('/srv/xrootd-metrics')
+        return Response(content, status=200, content_type='text/plain; version=0.0.4')
+    return Response("File not found", status=404, content_type='text/plain')
